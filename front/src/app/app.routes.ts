15c49a3303eb99672dc851ed7d2f3807f49ce77b
nareio <much.lio@laposte.net>
2024-12-31 15:35:26 +0100

    implementation header fonctionnel et routes angular

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { IndexPageComponent } from './index-page/index-page.component';
+import { AnalysePageComponent } from './analyse-page/analyse-page.component';
+import { RecherchePageComponent } from './recherche-page/recherche-page.component';
+import { PresentationPageComponent } from './presentation-page/presentation-page.component';
+import { DynamiquePageComponent } from './dynamique-page/dynamique-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: IndexPageComponent,
+      },
+      {
+        path: 'analyses',
+        component: AnalysePageComponent,
+      },
+      {
+        path: 'recherche',
+        component: RecherchePageComponent,
+      },
+      {
+        path: 'presentation',
+        component: PresentationPageComponent,
+      },
+      {
+        path: 'dynamique',
+        component: DynamiquePageComponent,
+      },
+];
