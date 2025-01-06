676f53b7390790ecf04b42a030779ec07d793208
Baptiste-B14 <blanchet.baptiste77@gmail.com>
2025-01-06 23:32:06 +0100

    modal sur kpi

--- front/src/app/kpi/kpi.component.ts
+++ front/src/app/kpi/kpi.component.ts
@@ -12,19 +12,20 @@ import { SingleData } from '../types';
 })
 export class KPIComponent implements OnInit {
   cardColor = "white";
-  colorScheme = "white";
-    
+  isModalOpen = false;
+  selectedKpi: { name: string; value: number; info: string } | null = null;
+
   result: SingleData = [];
 
   kpiData = [
-    { name: 'KPI 1', value: 40 },
-    { name: 'KPI 2', value: 90 },
-    { name: 'KPI 3', value: 50 },
-    { name: 'KPI 4', value: 100 },
-    { name: 'KPI 5', value: 75 },
-    { name: 'KPI 6', value: 60 },
-    { name: 'KPI 7', value: 85 },
-    { name: 'KPI 8', value: 55 },
+    { name: 'KPI 1', value: 40, info: 'Informations supplémentaires sur KPI 1' },
+    { name: 'KPI 2', value: 90, info: 'Informations supplémentaires sur KPI 2' },
+    { name: 'KPI 3', value: 50, info: 'Informations supplémentaires sur KPI 3' },
+    { name: 'KPI 4', value: 100, info: 'Informations supplémentaires sur KPI 4' },
+    { name: 'KPI 5', value: 75, info: 'Informations supplémentaires sur KPI 5' },
+    { name: 'KPI 6', value: 60, info: 'Informations supplémentaires sur KPI 6' },
+    { name: 'KPI 7', value: 85, info: 'Informations supplémentaires sur KPI 7' },
+    { name: 'KPI 8', value: 55, info: 'Informations supplémentaires sur KPI 8' },
   ];
 
   constructor(private readonly graphService: GraphService) {}
@@ -35,5 +36,18 @@ export class KPIComponent implements OnInit {
 
   private printResult() {
     this.result = this.kpiData;
+  }
+
+  onKpiSelect(kpi: { name: string }) {
+    const selected = this.kpiData.find((item) => item.name === kpi.name);
+    if (selected) {
+      this.selectedKpi = selected;
+      this.isModalOpen = true;
+    }
+  }
+
+  closeKpiModal() {
+    this.isModalOpen = false;
+    this.selectedKpi = null;
   }
 }
