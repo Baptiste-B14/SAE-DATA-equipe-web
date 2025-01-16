b3ab97c33f21d990738b3e119d4026156f87ff7d
Lucass-Dev <liger.lucas33@gmail.com>
2025-01-16 17:12:49 +0100

    ajout route top_collab

--- back/app.py
+++ back/app.py
@@ -29,13 +29,22 @@ flask_cors.CORS(app, resources={
 
 @app.route('/top_collab', methods=['GET'])
 def top_collab() :
+    period = request.args.get('period', default='all_time', type=str)
     limit = request.args.get('limit', default=50, type=int)
-    answer = execute_query("MATCH (a:Personne)-[r]->(p:Publication)<-[r2]-(b:Personne) WHERE a <> b AND id(a) < id(b) return a.nom, count(r2) AS count ORDER BY count DESC LIMIT " + limit)
-    return {"Message": answer}, 200
-
-@app.route('/collab_by_year', methods=['GET'])
-def collab_by_year():
-    answer = execute_query("MATCH (a:Personne)-[r]->(p:Publication)<-[r2]-(b:Personne) WHERE a <> b AND id(a) < id(b) WITH p.year AS year, COUNT(r2) AS relations_count RETURN year, relations_count ORDER BY year DESC;")
+    answer = None
+    if period == "all_time":
+        answer = execute_query("MATCH (a:Person)-[r]->(p:Publication)<-[r2]-(b:Person) WHERE a <> b AND id(a) < id(b) return a.person_name, count(r2) AS count ORDER BY count DESC LIMIT" + str(limit))
+    elif period == "before":
+        answer = execute_query(
+            "MATCH (a:Person)-[r]->(p:Publication)<-[r2]-(b:Person) WHERE a <> b AND id(a) < id(b) AND p.year > 2015 AND p.year < 2019 return a.person_name, count(r2) AS count ORDER BY count DESC LIMIT " + str(limit))
+    elif period == "during":
+        answer = execute_query(
+            "MATCH (a:Person)-[r]->(p:Publication)<-[r2]-(b:Person) WHERE a <> b AND id(a) < id(b) AND p.year > 2018 AND p.year < 2023 return a.person_name, count(r2) AS count ORDER BY count DESC LIMIT " + str(
+                limit))
+    elif period == "after":
+        answer = execute_query(
+            "MATCH (a:Person)-[r]->(p:Publication)<-[r2]-(b:Person) WHERE a <> b AND id(a) < id(b) AND p.year > 2022 return a.person_name, count(r2) AS count ORDER BY count DESC LIMIT " + str(
+                limit))
 
     return {"message" : answer}, 200
 
@@ -46,7 +55,7 @@ def execute():
 
 @app.route('/graph_collab', methods=['GET'])
 def graph_collab():
-    answer = execute_query("MATCH (a:Personne)-[r]->(p:Publication)<-[r2]-(b:Personne) WHERE a <> b AND id(a) < id(b) WITH p, a, b, p.year AS annee RETURN a.nom, b.nom, annee")
+    answer = execute_query("MATCH (a:Person)-[r]->(p:Publication)<-[r2]-(b:Person) WHERE a <> b AND id(a) < id(b) WITH p, a, b, p.year AS annee RETURN a.nom, b.nom, annee")
     nodes = []
     links = []
     for line in answer :
