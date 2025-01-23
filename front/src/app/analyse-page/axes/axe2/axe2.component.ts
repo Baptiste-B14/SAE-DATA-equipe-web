f66bb3eff9f494ca40e399260a220f28e6ca8999
Baptiste-B14 <blanchet.baptiste77@gmail.com>
2025-01-24 00:33:27 +0100

    change axe of graph

--- front/src/app/analyse-page/axes/axe2/axe2.component.ts
+++ front/src/app/analyse-page/axes/axe2/axe2.component.ts
@@ -6,16 +6,18 @@ import { WordCloudComponent} from "../../../word-cloud/word-cloud.component";
 import {WordChartComponent} from "../../../word-chart/word-chart.component";
 import {ForceGraphComponent} from "../../../graph/graph.component";
 import { LinechartCustomComponent} from "../../../linechart-custom/linechart-custom.component";
+import {LineChartComponent} from "../../../linechart-color/linechart-color.component";
+import {BarchartCustomComponent} from "../../../barchart-custom/barchart-custom.component";
 
 
 @Component({
   selector: 'app-axe2',
   standalone: true,
-    imports: [BarChartModule,
-        WordCloudComponent,
-        WordChartComponent, ForceGraphComponent,
-        LinechartCustomComponent
-    ],
+  imports: [BarChartModule,
+    WordCloudComponent,
+    WordChartComponent, ForceGraphComponent,
+    LinechartCustomComponent, LineChartComponent, BarchartCustomComponent
+  ],
   templateUrl: './axe2.component.html',
   styleUrl: './axe2.component.scss'
 })
